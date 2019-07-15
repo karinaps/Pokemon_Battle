5be20287355494dff7714624461e1bb216ffea4a
karinaps <karinaprmtsari@gmail.com>
2019-07-16 00:29:22 +0700

    2nd commit

--- 3a-model.py
+++ 3a-model.py
@@ -1,19 +1,91 @@
+import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
-df=pd.read_csv('datatrain.csv')
+dfPokemon=pd.read_csv('pokemon.csv')
+dfCombat=pd.read_csv('combats.csv')
+# print(dfPokemon)
+# print(dfCombat.values[0])
+# print(len(dfCombat.values))
 
-var_x=df.drop(['Unnamed: 0','idpoke1','idpoke2','winner'],axis=1)
-print(var_x.columns.values)
-var_y=df['winner']
+data_match=pd.DataFrame(columns=[
+    'HP1','Attack1','Defense1','Special Attack1',
+    'Special Defense1','Speed1','HP2','Attack2',
+    'Defense2','Special Attack2','Special Defense2','Speed2']
+)
+j=0
+for i in dfCombat.values:
+    data_match.loc[j]=(
+        dfPokemon[dfPokemon['#']==i[0]]['HP'].values[0],
+        dfPokemon[dfPokemon['#']==i[0]]['Attack'].values[0],
+        dfPokemon[dfPokemon['#']==i[0]]['Defense'].values[0],
+        dfPokemon[dfPokemon['#']==i[0]]['Sp. Atk'].values[0],
+        dfPokemon[dfPokemon['#']==i[0]]['Sp. Def'].values[0],
+        dfPokemon[dfPokemon['#']==i[0]]['Speed'].values[0],
+        dfPokemon[dfPokemon['#']==i[1]]['HP'].values[0],
+        dfPokemon[dfPokemon['#']==i[1]]['Attack'].values[0],
+        dfPokemon[dfPokemon['#']==i[1]]['Defense'].values[0],
+        dfPokemon[dfPokemon['#']==i[1]]['Sp. Atk'].values[0],
+        dfPokemon[dfPokemon['#']==i[1]]['Sp. Def'].values[0],
+        dfPokemon[dfPokemon['#']==i[1]]['Speed'].values[0])
+    j+=1
+    if j==1000:
+        break
+    # print(data_match.iloc[0])
+    # break
 
-#splitting to data training and data testing
-from sklearn.model_selection import train_test_split
-xtrain,xtest,ytrain,ytest=train_test_split(var_x,var_y,test_size=0.1)
+j=0
+for i in dfCombat['Winner']:
+    if i==dfCombat.iloc[j]['First_pokemon']:
+        dfCombat.iloc[j]['Winner']=1
+        j+=1
+    else:
+        dfCombat.iloc[j]['Winner']=0
+        j+=1
+target=dfCombat['Winner'].iloc[0:1000]
 
-model=LogisticRegression(solver='liblinear',multi_class='auto')
-model.fit(xtrain,ytrain)
-print(model.score(xtest,ytest))  #89,28%
+# print(target)
+# print(data_match)
+# print(np.matrix(data_match))
 
-import joblib
-joblib.dump(model,'modeljoblib')
+dfx = np.matrix(data_match)
+dfy = target
+
+#=============================================================== MODEL MACHINE LEARNING ========================================================================================================================================
+
+from sklearn.ensemble import RandomForestClassifier
+modelRandom=RandomForestClassifier()
+modelRandom.fit(dfx,dfy)
+
+poke1='Pikachu'
+poke2='Charizard'
+indexpoke1=dfPokemon[dfPokemon['Name']==poke1]['#'].values[0]
+indexpoke2=dfPokemon[dfPokemon['Name']==poke2]['#'].values[0]
+
+statpoke=[]
+statpoke.append([
+    dfPokemon[dfPokemon['#']==indexpoke1]['HP'].values[0],
+    dfPokemon[dfPokemon['#']==indexpoke1]['Attack'].values[0],
+    dfPokemon[dfPokemon['#']==indexpoke1]['Defense'].values[0],
+    dfPokemon[dfPokemon['#']==indexpoke1]['Sp. Atk'].values[0],
+    dfPokemon[dfPokemon['#']==indexpoke1]['Sp. Def'].values[0],
+    dfPokemon[dfPokemon['#']==indexpoke1]['Speed'].values[0],
+    dfPokemon[dfPokemon['#']==indexpoke2]['HP'].values[0],
+    dfPokemon[dfPokemon['#']==indexpoke2]['Attack'].values[0],
+    dfPokemon[dfPokemon['#']==indexpoke2]['Defense'].values[0],
+    dfPokemon[dfPokemon['#']==indexpoke2]['Sp. Atk'].values[0],
+    dfPokemon[dfPokemon['#']==indexpoke2]['Sp. Def'].values[0],
+    dfPokemon[dfPokemon['#']==indexpoke2]['Speed'].values[0]])
+# print(statpoke)
+
+print(modelRandom.predict_proba(statpoke))
+print(modelRandom.predict(statpoke))
+proba=modelRandom.predict_proba(statpoke)
+
+if proba[0][0]>=proba[0][1]:
+    print(str(proba[0][0]*100)+'%',str(poke2),'Wins!')
+else:
+    print(str(proba[0][1]*100)+'%',str(poke2),'Wins!')
+
+import joblib as jb
+jb.dump(modelRandom,'modelPokemon')
--- 3b-server.py
+++ 3b-server.py
@@ -1,76 +1,150 @@
-from flask import redirect, request, Flask, render_template, url_for
-import json, requests
+import numpy as np
 import pandas as pd
-import joblib
+from flask import redirect, request, Flask, render_template, send_from_directory
+import json, requests
+import joblib as jb
+import matplotlib.pyplot as plt
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config['upload_folder']='album'
 
 # HOME ROUTE =====================================================================================================================
-
 @app.route('/')
 def home():
     return render_template('1ujian.html')
 
+# HASIL & UPLOAD ROUTE ==============================================================================================================================
 
-# HASIL ROUTE ==============================================================================================================================
-@app.route('/hasil', methods=['POST','GET'])
+@app.route('/hasil', methods=['POST'])
 def post():
-    model=joblib.load('modeljoblib')
-
-    poke1=request.form['namapokemon1']
-    poke2=request.form['namapokemon2']
-    poke1=poke1.lower() #nama
-    poke2=poke2.lower() #nama
-
-    datapokemon=pd.read_csv('pokemon.csv')
-    idpoke1=(datapokemon[datapokemon['Name']==poke1.title()].index.values[0])+1
-    idpoke2=(datapokemon[datapokemon['Name']==poke2.title()].index.values[0])+1
-
+    poke1=request.form['namapokemon1'].lower()
+    poke2=request.form['namapokemon2'].lower()
+    
     url1='https://pokeapi.co/api/v2/pokemon/'+poke1
     url2='https://pokeapi.co/api/v2/pokemon/'+poke2
-    data1=requests.get(url1)
-    data2=requests.get(url2)
+    
+    pokemon1 = requests.get(url1)
+    pokemon2 = requests.get(url2)
+    
+    if str(pokemon1)=='<Response [404]>' or str(pokemon2)=='<Response [404]>':
+        return redirect('/NotFound')
 
-    if str(data1)=='<Response [404]>':
-        return redirect(url_for('notfound'))
-    if str(data2)=='<Response [404]>':
-        return redirect(url_for('notfound'))
-
-    gambar1=data1.json()['sprites']['front_default']
-    gambar2=data2.json()['sprites']['front_default']
-
-# =============================================== from pokemon.csv
-    dfpoke=pd.read_csv('pokemon.csv',index_col=0)
-
-    hp1=dfpoke.loc[idpoke1]['HP']
-    hp2=dfpoke.loc[idpoke2]['HP']
-    attack1=dfpoke.loc[idpoke1]['Attack']
-    attack2=dfpoke.loc[idpoke2]['Attack']
-    defense1=dfpoke.loc[idpoke1]['Defense']
-    defense2=dfpoke.loc[idpoke2]['Defense']
-    spatk1=dfpoke.loc[idpoke1]['Sp. Atk']
-    spatk2=dfpoke.loc[idpoke2]['Sp. Atk']
-    spdef1=dfpoke.loc[idpoke1]['Sp. Def']
-    spdef2=dfpoke.loc[idpoke2]['Sp. Def']
-    speed1=dfpoke.loc[idpoke1]['Speed']
-    speed2=dfpoke.loc[idpoke2]['Speed']
-
-    predict=model.predict([[hp1,hp2,attack1,attack2,defense1,defense2,spatk1,spatk2,spdef1,spdef2,speed1,speed2]])[0]
-    if predict==0:
-        winner=poke1.title()
+    name1 = pokemon1.json()['name']
+    upper1 = name1[0].upper()
+    nama1 = upper1 + name1[1:]
+    
+    name2 = pokemon2.json()['name']
+    upper2 = name2[0].upper()
+    nama2 = upper2 + name2[1:]
+    
+    dfPokemon = pd.read_csv('pokemon.csv')
+    # dfCombat = pd.read_csv('combats.csv')
+    indexpoke1 = dfPokemon[dfPokemon['Name']==nama1]['#'].values[0]
+    indexpoke2 = dfPokemon[dfPokemon['Name']==nama2]['#'].values[0]
+    
+    statpoke=[]
+    statpoke.append([
+        dfPokemon[dfPokemon['#']==indexpoke1]['HP'].values[0],
+        dfPokemon[dfPokemon['#']==indexpoke1]['Attack'].values[0],
+        dfPokemon[dfPokemon['#']==indexpoke1]['Defense'].values[0],
+        dfPokemon[dfPokemon['#']==indexpoke1]['Sp. Atk'].values[0],
+        dfPokemon[dfPokemon['#']==indexpoke1]['Sp. Def'].values[0],
+        dfPokemon[dfPokemon['#']==indexpoke1]['Speed'].values[0],
+        dfPokemon[dfPokemon['#']==indexpoke2]['HP'].values[0],
+        dfPokemon[dfPokemon['#']==indexpoke2]['Attack'].values[0],
+        dfPokemon[dfPokemon['#']==indexpoke2]['Defense'].values[0],
+        dfPokemon[dfPokemon['#']==indexpoke2]['Sp. Atk'].values[0],
+        dfPokemon[dfPokemon['#']==indexpoke2]['Sp. Def'].values[0],
+        dfPokemon[dfPokemon['#']==indexpoke2]['Speed'].values[0]])
+    
+    model = jb.load('modelPokemon')
+    proba=model.predict_proba(statpoke)
+    if proba[0][0]>=proba[0][1]:
+        win = str(proba[0][0]*100)+'%'+' '+str(nama2)+' '+'Wins!'
     else:
-        winner=poke2.title()
+        win = str(proba[0][1]*100)+'%'+' '+str(nama1)+' '+'Wins!'
+        
+    poke=[nama1,nama2]
+    pokehp=[dfPokemon[dfPokemon['#']==indexpoke1]['HP'].values[0],dfPokemon[dfPokemon['#']==indexpoke2]['HP'].values[0]]
+    pokeatk=[dfPokemon[dfPokemon['#']==indexpoke1]['Attack'].values[0],dfPokemon[dfPokemon['#']==indexpoke2]['Attack'].values[0]]
+    pokedef=[dfPokemon[dfPokemon['#']==indexpoke1]['Defense'].values[0],dfPokemon[dfPokemon['#']==indexpoke2]['Defense'].values[0]]
+    pokespatk=[dfPokemon[dfPokemon['#']==indexpoke1]['Sp. Atk'].values[0],dfPokemon[dfPokemon['#']==indexpoke2]['Sp. Atk'].values[0]]
+    pokespdef=[dfPokemon[dfPokemon['#']==indexpoke1]['Sp. Def'].values[0],dfPokemon[dfPokemon['#']==indexpoke2]['Sp. Def'].values[0]]
+    pokespd=[dfPokemon[dfPokemon['#']==indexpoke1]['Speed'].values[0],dfPokemon[dfPokemon['#']==indexpoke2]['Speed'].values[0]]
 
-    proba=model.predict_proba([[hp1,hp2,attack1,attack2,defense1,defense2,spatk1,spatk2,spdef1,spdef2,speed1,speed2]])
-    probamax=round(proba[0,predict]*100)
+    # plotting to figures
+    # fig = plt.figure(figsize=(13,9))
+    plt.subplot(161)
+    plt.bar(poke,pokehp,color=['lightblue','lightgreen'])
+    plt.title('HP')
+    i=0
 
-    return render_template('2ujian.html',nama1=poke1.title(),nama2=poke2.title(),gambar1=gambar1,gambar2=gambar2,winner=winner,proba=probamax)
+    while i<len(poke):
+        plt.text(poke[i],pokehp[i]-4,f'''{pokehp[i]}''',fontsize=17)
+        i+=1
 
+    plt.subplot(162)
+    plt.bar(poke,pokeatk,color=['lightblue','lightgreen'])
+    plt.title('Attack')
+    i=0
+    while i<len(poke):
+        plt.text(poke[i],pokeatk[i]-4,f'''{pokeatk[i]}''',fontsize=17)
+        i+=1
+
+    plt.subplot(163)
+    plt.bar(poke,pokedef,color=['lightblue','lightgreen'])
+    plt.title('Defense')
+    i=0
+    while i<len(poke):
+        plt.text(poke[i],pokedef[i]-4,f'''{pokedef[i]}''',fontsize=17)
+        i+=1
+
+    plt.subplot(164)
+    plt.bar(poke,pokespatk,color=['lightblue','lightgreen'])
+    plt.title('Special Attack')
+    i=0
+    while i<len(poke):
+        plt.text(poke[i],pokespatk[i]-4,f'''{pokespatk[i]}''',fontsize=17)
+        i+=1
+
+    plt.subplot(165)
+    plt.bar(poke,pokespdef,color=['lightblue','lightgreen'])
+    plt.title('Special Defense')
+    i=0
+    while i<len(poke):
+        plt.text(poke[i],pokespdef[i]-4,f'''{pokespdef[i]}''',fontsize=17)
+        i+=1
+
+    plt.subplot(166)
+    plt.bar(poke,pokespd,color=['lightblue','lightgreen'])
+    plt.title('Speed')
+    i=0
+    while i<len(poke):
+        plt.text(poke[i],pokespd[i]-4,f'''{pokespd[i]}''',fontsize=17)
+        i+=1
+
+    addressgrafik='./album/'+nama1+'vs'+nama2+'.png'
+    urlgrafik='http://localhost:5000/fileupload/'+nama1+'vs'+nama2+'.png'
+    plt.savefig(addressgrafik)
+    grafik=urlgrafik
+    
+    filegmb1 = pokemon1.json()['sprites']
+    gmb1 = filegmb1['front_default']
+
+    filegmb2 = pokemon2.json()['sprites']
+    gmb2 = filegmb2['front_default']
+
+    files = [nama1, nama2, gmb1, gmb2, win, grafik]
+    return render_template('2ujian.html',x=files)
+
+@app.route('/fileupload/<path:x>')
+def hasilUpload(x):
+    return send_from_directory('storage',x)
 
 # ERROR & NOT FOUND ROUTE ========================================================================================================================================
 
-@app.route('/notFound')
-def notfound():
+@app.route('/NotFound')
+def notFound():
     return render_template('errorujian.html')
 
 @app.errorhandler(404)
